9bffe5274fb10683ff6fffa097c3051be6e22c5c
RenoAverill <reno_averill@yahoo.com>
2021-05-24 14:35:23 -0600

    my completed 99-bottles.js

--- 99_bottles.js
+++ 99_bottles.js
@@ -1,5 +1,15 @@
 function bottleSong() {
-  // Write your code here!
+  let bottles = 99;
+  while (bottles > 1) {
+    console.log(`${bottles} bottles of beer on the wall, ${bottles} of beer.`)
+    console.log(`Take one down and pass it around, ${bottles} of beer on the wall.`)
+    bottles--
+  }
+  console.log('Take one down and pass it around, 1 bottle of beer on the wall.')
+  console.log('1 bottle of beer on the wall, 1 bottle of beer.')
+  console.log('Take one down and pass it around, no more bottles of beer on the wall.')
+  console.log('No more bottles of beer on the wall, no more bottles of beer.')
+  console.log('Go to the store and buy some more, 99 bottles of beer on the wall.')
 };
 
 bottleSong();
