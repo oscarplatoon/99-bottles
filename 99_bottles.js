67c03985be728ba4b4b0f062f7bba7e8392ff36f
ThomasVirn <thomas.virnston1@gmail.com>
2021-05-24 16:19:25 -0500

    solution

--- 99_bottles.js
+++ 99_bottles.js
@@ -7,7 +7,7 @@ function bottleSong(number) {
   } if (bottleAmount == 2) {
     console.log(`${bottleAmount} ${bottlePlural} of beer on the wall, ${bottleAmount} ${bottlePlural} of beer. Take one down and pass it around, ${bottleAmount -1} bottle of beer on the wall. \n 1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, no more bottles of beer on the wall. No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.`)
   } 
-  // Write your code here!
+ 
 };
 
 bottleSong(99);
