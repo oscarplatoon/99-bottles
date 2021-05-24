c6d7465db5e1ab72cfdef730a6187ee46945d62f
JWShuff <16825267+JWShuff@users.noreply.github.com>
2021-05-24 16:38:16 -0400

    Initial response without additional challenges. Test for git push

--- 99_bottles.js
+++ 99_bottles.js
@@ -1,5 +1,27 @@
 function bottleSong() {
-  // Write your code here!
-};
 
+  //Use a for loop to process the bottles
+  for (let i = 99; i >= 0; i --) {
+
+    //if case for plural bottles
+    if (i > 2) {
+      console.log(i.toString() + " bottles of beer on the wall, " + i.toString() + " bottles of beer.")
+      console.log("Take one down and pass it around, " + (i-1).toString() + " bottles of beer on the wall.")
+
+      //if case for second bottles being singular
+    } if (i == 2) {
+      console.log(i.toString() + " bottles of beer on the wall, " + i.toString() + " bottles of beer.") 
+      console.log("Take one down and pass it around, " + (i-1).toString() + " bottle of beer on the wall.")
+      //if case for second set of bottles being 0
+    } if(i == 1) {
+      console.log(i.toString() + " bottle of beer on the wall, " + i.toString() + " bottle of beer.") 
+      console.log("Take one down and pass it around, " + "no more bottles of beer on the wall.")
+
+      //if case for no bottles of beer.
+    } else if (i == 0) {
+      console.log("No more bottles of beer on the wall, no more bottles of beer.")
+      console.log("Go to the store and buy some more, 99 bottles of beer on the wall.")
+    }
+  }
+}
 bottleSong();
