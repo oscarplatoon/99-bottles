954cbbee5ed7fee95606bbbd98c65e2b5f21152f
Jeremiah Mauga <miahmauga@gmail.com>
2021-05-24 13:50:46 -0700

    Jeremiah's 99 Bottles coding challenge completed

--- 99_bottles.js
+++ 99_bottles.js
@@ -1,5 +1,11 @@
-function bottleSong() {
-  // Write your code here!
+function bottleSong(num) {
+  for (let i = num; i > 0; i--) {
+    if (i >= 0) {
+      console.log((i) + " bottles of beer on the wall, " + (i) + " bottles of beer. Take one down and pass it around, " + (i - 1) + " bottles of beer on the wall.");
+    }
+  }
 };
 
-bottleSong();
+bottleSong(99);
+
+
