cb1d7faee0c2c1f9b8e6e052a9f771f5be5c2e5d
Marc Stanley <marcson909@gmail.com>
2021-05-24 18:28:22 -0500

    completed 99 bottles challenge with variable to allow for various bottle amounts above 0

--- 99_bottles.js
+++ 99_bottles.js
@@ -1,5 +1,19 @@
-function bottleSong() {
+function bottleSong(num) {
   // Write your code here!
+  let bottles = num;
+  while (bottles > 1){
+    console.log(`${bottles} bottles of beer on the wall, ${bottles} bottles of beer.`);
+    bottles--;
+    if (bottles == 1){
+      console.log(`Take one down and pass it around, ${bottles} bottle of beer on the wall.`);
+    } else {
+      console.log(`Take one down and pass it around, ${bottles} bottles of beer on the wall.`);
+    }
+  }
+  if (bottles == 1) {
+    console.log(`${bottles} bottle of beer on the wall, ${bottles} bottle of beer.\nTake one down and pass it around, no more bottles of beer on the wall.\nNo more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.`);
+}
+
 };
 
-bottleSong();
+bottleSong(99);
