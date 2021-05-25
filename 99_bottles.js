5e099434936b91afc32abb5d6e4886d7c15f07cd
Tianlin Cai <terrylovesbird@users.noreply.github.com>
2021-05-25 05:11:27 +0000

    to be solved

--- 99_bottles.js
+++ 99_bottles.js
@@ -1,5 +1,5 @@
 function bottleSong() {
-  // Write your code here!
+  console.log("just finished the installfest at 23:50pm, and need to go to sleep. My solution would be a for-loop")
 };
 
 bottleSong();
