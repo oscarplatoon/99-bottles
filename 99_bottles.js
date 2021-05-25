3884969ae8860dc750a8fce83b24b01dcef21ae2
RenoAverill <reno_averill@yahoo.com>
2021-05-25 08:50:28 -0600

    updated 99_bottles.js

--- 99_bottles.js
+++ 99_bottles.js
@@ -1,15 +1,14 @@
-function bottleSong() {
-  let bottles = 99;
+function bottleSong(num) {
+  let bottles = num;
   while (bottles > 1) {
     console.log(`${bottles} bottles of beer on the wall, ${bottles} of beer.`)
-    console.log(`Take one down and pass it around, ${bottles} of beer on the wall.`)
+    console.log(`Take one down and pass it around, ${bottles -1} bottles of beer on the wall.`)
     bottles--
   }
-  console.log('Take one down and pass it around, 1 bottle of beer on the wall.')
   console.log('1 bottle of beer on the wall, 1 bottle of beer.')
   console.log('Take one down and pass it around, no more bottles of beer on the wall.')
   console.log('No more bottles of beer on the wall, no more bottles of beer.')
-  console.log('Go to the store and buy some more, 99 bottles of beer on the wall.')
+  console.log(`Go to the store and buy some more, ${num} bottles of beer on the wall.`)
 };
 
-bottleSong();
+bottleSong(5);
--- 99_bottles.py
+++ 99_bottles.py
@@ -1,5 +1,8 @@
 def bottle_song():
-	# write your code here!
-
+	bottles = 99
+	while (bottles > 1)
+		print(bottles + ' bottles of beer on the wall, ' + bottles + ' of beer.')
+		print('take one down and pass it around, ' bottles + ' bottles of beer on the wall.')
+		bottles=- 1
 
 bottle_song()
