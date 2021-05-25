5dcbe9a22f1cfaa1cdfc34fb9974a9e9433b4832
Jeremiah Mauga <miahmauga@gmail.com>
2021-05-25 07:44:59 -0700

    Jeremiah's 99-bottles solution

--- 99_bottles.js
+++ 99_bottles.js
@@ -1,9 +1,15 @@
 function bottleSong(num) {
+  
+  
+  
   for (let i = num; i > 0; i--) {
     if (i >= 0) {
-      console.log((i) + " bottles of beer on the wall, " + (i) + " bottles of beer. Take one down and pass it around, " + (i - 1) + " bottles of beer on the wall.");
-    }
+      console.log(`${i} bottle${i > 1 ? 's' : '' } of beer on the wall, ${i} bottles of beer. Take one down and pass it around, ${i - 1} bottle${i - 1 > 1 ? 's' : '' } of beer.`);
+    }  
   }
+  console.log("Take one down and pass it around, no more bottles of beer on the wall.");
+  console.log("No more bottles of beer on the wall, no more bottles of beer.");
+  console.log(`Go to the store buy some more, ${num} bottles of beer on the wall.`)
 };
 
 bottleSong(99);
