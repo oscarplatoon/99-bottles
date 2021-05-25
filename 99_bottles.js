b8530e5566ad76af56ae6b0118dd11db1bc906f1
stesun2 <ss27@illinois.edu>
2021-05-25 07:33:45 -0500

    completed 99-bottles

--- 99_bottles.js
+++ 99_bottles.js
@@ -1,24 +1,27 @@
 function bottleSong(num) {
   // Write your code here!
-  // Testing
   // cases > 1 beer
+  // cases = 1 beer
   // cases =  0 beer
 
-  for (i = num; num > 0; num--){
+  for (i = num; num >= 0; num--){
     if (num == 0) {
-      console.log('No more bottles of beer on the wall, no more bottles of beer.\n')
-      console.log('Go to the store and buy some more, 99 bottles of beer on the wall.\n')
+      console.log('No more bottles of beer on the wall, no more bottles of beer.')
+      console.log('Go to the store and buy some more, 99 bottles of beer on the wall.')
     }
+    else if (num == 1) {
+      console.log(num,'bottles of beer on the wall,',
+      num, 'bottles of beer.')
+      console.log('Take one down and pass it around, no more bottles of beer on the wall.')
+  
+    } 
     else {
       console.log(num,'bottles of beer on the wall,',
       num, 'bottles of beer.')
-      console.log('Take one down and pass it around,', num-1,' bottles of beer \non the wall.')
+      console.log('Take one down and pass it around,', num-1,' bottles of beer on the wall.')
       
     }
-
   }
-  
-
 };
 
-bottleSong(12);
+bottleSong(1);
