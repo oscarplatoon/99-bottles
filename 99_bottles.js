32cfe2de04647c29a7e6a7e1f6c44b044f270324
courtneyhussein <courtneyhussein124@gmail.com>
2021-05-25 00:26:51 -0400

    My 99 Bottles Solution

--- 99_bottles.js
+++ 99_bottles.js
@@ -1,5 +1,20 @@
 function bottleSong() {
-  // Write your code here!
+  let bottles = 99
+
+  for (let i = bottles; i > 0; i--){
+
+    if (i === 2){
+      console.log("2 bottles of beer on the wall, 2 bottles of beer. Take one down and pass it around, 1 bottle of beer on the wall. ")
+    }  
+    else if (i === 1){
+      console.log(`1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, no more bottles of beer on the wall.\n No more bottles of beer on the wall,no more bottles of beer. Go to the store and buy some more, ${bottles} bottles of beer on the wall.`)
+    }  
+    else {
+
+     console.log(`${i} bottles of beer on the wall, ${i} bottles of beer. Take one down and pass it around, ${i - 1} bottles of beer on the wall.`)
+   }
+}
+
 };
 
 bottleSong();
