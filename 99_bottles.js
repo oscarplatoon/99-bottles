0036f3a9078b52b19b5481c8e243da36a1315409
jung341416 <jung341416@gmail.com>
2021-05-26 01:56:41 -0400

    last commit

--- 99_bottles.js
+++ 99_bottles.js
@@ -1,20 +1,18 @@
-
 function bottleSong(num) {
   // Write your code here!\
-  let lyrics = "";
-  for (let i = num; i >= 0; i--) { 
-    if (i > 2) {
-    lyrics = `${i} bottles of beer on the Wall, ${i} bottles of beer.\nTake one down and pass it around, ${i - 1} bottles of beer on the wall.`;
-    } else if (i === 2){
-    lyrics = `${i} bottles of beer on the Wall, ${i} bottles of beer.\nTake one down and pass it around, ${i - 1} bottle of beer on the wall.`;
-    } else if (i === 1) {
-    lyrics =  `1 bottle of beer on the wall, 1 bottle of beer.\nTake one down and pass it around, no more bottles of beer on the wall.`;
+  let bottleNum = num;
+  while (bottleNum >= 0) {
+    let s = bottleNum > 1 ? 's' : '';
+    let bottleMinusOneS = bottleNum - 1 > 1? 's' : '';
+    let str = '';
+    if (bottleNum > 0) {
+    str += `${bottleNum} bottle${s} of beer on the Wall, ${bottleNum} bottle${s} of beer.\nTake one down and pass it around, ${bottleNum-1} bottle${bottleMinusOneS} of beer on the wall.`;
     } else {
-    lyrics = `No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.`
+    str += `No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, ${num} bottles of beer on the wall.`;
     }
-    console.log(lyrics);
-  
-};
+    console.log(str);
+    bottleNum --;
+  }
 }
 
 bottleSong(10);
