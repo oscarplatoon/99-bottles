08d2ea8ada65eefc8a97fc005b87e03118eefd3c
stesun2 <ss27@illinois.edu>
2021-05-26 14:39:42 -0500

    completed 99-bottles in py

--- 99_bottles.js
+++ 99_bottles.js
@@ -10,18 +10,17 @@ function bottleSong(num) {
       console.log('Go to the store and buy some more, 99 bottles of beer on the wall.')
     }
     else if (num == 1) {
-      console.log(num,'bottles of beer on the wall,',
+      console.log(num,'bottle of beer on the wall,',
       num, 'bottles of beer.')
       console.log('Take one down and pass it around, no more bottles of beer on the wall.')
   
     } 
     else {
-      console.log(num,'bottles of beer on the wall,',
-      num, 'bottles of beer.')
-      console.log('Take one down and pass it around,', num-1,' bottles of beer on the wall.')
+      console.log(`${num} bottles${num > 1 ? 's' : ''} of beer on the wall, ${num-1}bottle${num > 1 ? 's' : ''} of beer.`)
+      console.log(`Take one down and pass it around, ${num-1} bottle of beer on the wall.`)
       
     }
   }
 };
 
-bottleSong(1);
+bottleSong(5);
--- 99_bottles.py
+++ 99_bottles.py
@@ -1,5 +1,21 @@
-def bottle_song():
+def bottle_song(num):
 	# write your code here!
+	bottles = num
+	while(bottles > 1):
+		print(f'{bottles} bottles of beer on the wall, {bottles} bottles of beer.')
+		print(f'Take one down and pass it around, {bottles - 1} bottles of beer on the wall.')
+		bottles -= 1
 
-
-bottle_song()
+	if (bottles == 1):
+		print(f'{bottles} bottle of beer on the wall, {bottles} bottle of beer.')
+		print('Take one down and pass it around, no more bottles of beer on the wall.')
+		bottles -= 1
+	
+	if (bottles == 0):
+		print('No more bottles of beer on the wall, no more bottles of beer.')
+		print('Go to the store and buy some more, 99 bottles of beer on the wall.')
+		
+# bottle_song(0)
+# bottle_song(-1)
+# bottle_song(99)
+# bottle_song(3)
