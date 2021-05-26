5d179a8803b42f74fb1669f29f771eb7cdaee7ba
Marc Stanley <marcson909@gmail.com>
2021-05-26 15:46:11 -0500

    solved 99 bottles with while loop and if statements

--- 99_bottles.py
+++ 99_bottles.py
@@ -1,5 +1,28 @@
-def bottle_song():
+def bottle_song(n):
 	# write your code here!
+	bottles = n
+	while bottles > 1:
+		print(f"{bottles} bottles of beer on the wall, {bottles} bottles of beer.")
+		bottles -= 1
+		if bottles == 1:
+			print(f"Take one down and pass it around, {bottles} bottle of beer on the wall.")
+			break
+		print(f"Take one down and pass it around, {bottles} bottles of beer on the wall.")
+	if bottles == 1:
+		print(f"{bottles} bottle of beer on the wall, {bottles} bottle of beer.")
+		print(f"Take one down and pass it around, no more bottles of beer on the wall.")
+		bottles -= 1
+	if bottles == 0:
+		print(f"No more bottles of beer on the wall, no more bottles of beer.")
+		print(f"Go to the store and buy some more, {n} bottles of beer on the wall.")
+	
 
 
-bottle_song()
+bottle_song(99)
+
+# Take one down and pass it around, 1 bottle of beer on the wall.
+# 1 bottle of beer on the wall, 1 bottle of beer.
+# Take one down and pass it around, no more bottles of beer on the wall.
+# No more bottles of beer on the wall, no more bottles of beer.
+# Go to the store and buy some more, 99 bottles of beer on the wall.
+
