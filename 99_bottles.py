5f4a84aeac628b4e5e45deafaeb11d3ca5e87fa0
Jeremiah Mauga <miahmauga@gmail.com>
2021-05-26 12:36:52 -0700

    Jeremiah's python solution for 99_bottles

--- 99_bottles.py
+++ 99_bottles.py
@@ -1,5 +1,15 @@
 def bottle_song():
-	# write your code here!
+	
+	bottles = 99
+	plural = "bottles"
+	single = "bottle"
+
+	while bottles > 0:
+		print(f"{bottles} {single if bottles == 1 else plural} of beer on the wall, {bottles} {single if bottles == 1 else plural} of beer. You take one down, pass it around. {bottles - 1} {single if bottles - 1 == 1 else plural} of beer.")
+		bottles = bottles - 1
+	
+	print("No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.")
+		
 
 
 bottle_song()
