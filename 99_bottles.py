49d168f4fb1bb881bbf3669c4db3cf7cc0d80354
Jonas Paulikas <70077913+paulikas78@users.noreply.github.com>
2021-05-27 08:26:54 -0500

    updated 99_bottles.py

--- 99_bottles.py
+++ 99_bottles.py
@@ -1,5 +1,26 @@
-def bottle_song():
-	# write your code here!
+def bottle_song(x):
+    n = x
+    while n > 0:
+    	if n > 2:
+    		print(f"{n} bottles of beer on the wall.")
+      		print(f"{n} bottles of beer.")
+      		print("Take one down, pass it around,")
+      		print(f"{n - 1} bottles of beer on the wall.")
+    	elif n > 1:
+      		print(f"{n} bottles of beer on the wall.")
+      		print(f"{n} bottles of beer.")
+      		print("Take one down, pass it around,")
+      		print(f"{n - 1} bottle of beer on the wall.")
+    	else:
+      		print(f"{n} bottle of beer on the wall.")
+      		print(f"{n} bottle of beer.")
+      		print("Take one down, pass it around,")
+      		print("No more bottles of beer on the wall.")
+    	n -= 1 
+  	print("No more bottles of beer on the wall.")
+  	print("No more bottles of beer.")
+  	print("Go the store and buy some more,")
+  	print(f"{x} bottles of beer on the wall.")
 
 
-bottle_song()
+bottle_song(5)
